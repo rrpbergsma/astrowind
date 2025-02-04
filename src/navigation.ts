014cb34a10ac308d7d02c76278be4a81f03b87ff
becarta <github.devotee752@passmail.net>
2025-02-04 22:29:14 +0100

    updated socials

--- src/navigation.ts
+++ src/navigation.ts
@@ -21,10 +21,8 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'www.linkedin.com/in/rrpbergsma' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/becarta' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
 };
